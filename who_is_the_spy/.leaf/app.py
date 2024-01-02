26aa5528080d90cd0e406830ee9971fe3eb21279
PanQiWei <594557445@qq.com>
2024-01-02 16:43:15 +0800

    update app.py

--- who_is_the_spy/.leaf/app.py
+++ who_is_the_spy/.leaf/app.py
@@ -5,6 +5,7 @@ import requests
 import sys
 from argparse import ArgumentParser
 from contextlib import asynccontextmanager
+from functools import partial
 from threading import Thread
 from typing import Any, List, Optional
 from uuid import UUID
@@ -93,6 +94,15 @@ async def get_task_status() -> JSONResponse:
     return JSONResponse(content={"status": scene_engine.state.value})
 
 
+@app.get("/agents_connected")
+async def agents_connected() -> JSONResponse:
+    return JSONResponse(
+        content={
+            agent_id: agent.connected for agent_id, agent in scene_engine.scene.dynamic_agents.items()
+        }
+    )
+
+
 @app.websocket("/ws")
 async def stream_task_info(websocket: WebSocket) -> None:
     await websocket.accept()
@@ -113,11 +123,10 @@ async def validate_human(request: Request, call_next):
 @app.websocket("/ws/human/{agent_id}")
 async def human_input(websocket: WebSocket, agent_id: str):
     await human_conn_manager.connect(websocket, agent_id)
-    try:
-        while True:
-            await asyncio.sleep(0.1)
-    except WebSocketDisconnect:
-        human_conn_manager.disconnect(agent_id)
+    await scene_engine.socket_handler.stream_sockets(
+        websocket,
+        postprocess_on_disconnect=partial(human_conn_manager.disconnect, **{"agent_id": agent_id})
+    )
 
 
 @app.post("/save")
