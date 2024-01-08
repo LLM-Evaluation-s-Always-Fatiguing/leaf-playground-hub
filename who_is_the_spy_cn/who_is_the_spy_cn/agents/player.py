f1c0105b4a7496fa68c1312806951f9af872e486
PanQiWei <594557445@qq.com>
2024-01-08 15:32:44 +0800

    set action timeout to 120 seconds

--- who_is_the_spy_cn/who_is_the_spy_cn/agents/player.py
+++ who_is_the_spy_cn/who_is_the_spy_cn/agents/player.py
@@ -18,7 +18,8 @@ class BaseAIPlayer(
     SceneAIAgent,
     ABC,
     role_definition=ROLE_DEFINITION,
-    cls_description="An AI agent who participants in the game Who is the Spy as a player"
+    cls_description="An AI agent who participants in the game Who is the Spy as a player",
+    action_exec_timeout=120
 ):
     config_cls = BaseAIPlayerConfig
     config: config_cls
