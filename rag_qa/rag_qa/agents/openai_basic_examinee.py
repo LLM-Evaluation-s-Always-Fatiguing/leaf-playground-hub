ace491afad60c5b657957da2cbc5dc32fa00c12e
PanQiWei <594557445@qq.com>
2024-02-21 18:17:50 +0800

    refactor: evaluator and ai backends using third party dependencies

--- rag_qa/rag_qa/agents/openai_basic_examinee.py
+++ rag_qa/rag_qa/agents/openai_basic_examinee.py
@@ -1,16 +1,15 @@
 import json
+from typing import Literal, Type, Union
 
-from openai import AsyncOpenAI
 from openai.types.chat.chat_completion_message_param import (
     ChatCompletionSystemMessageParam,
     ChatCompletionUserMessageParam,
 )
 from pydantic import Field
 
-from leaf_playground.ai_backend.openai import OpenAIBackendConfig, CHAT_MODELS
-from leaf_playground.data.media import Text, Json
+from leaf_ai_backends.openai import OpenAIBackendConfig, OpenAIBackend, OpenAIClientConfig, AzureOpenAIClientConfig
+from leaf_playground.data.media import Json
 from leaf_playground.data.profile import Profile
-from leaf_playground.utils.import_util import DynamicObject
 
 from .base_examinee import (
     AIBaseExaminee,
@@ -19,12 +18,29 @@ from .base_examinee import (
 from ..scene_definition import ExamineeAnswer, ExaminerQuestion
 
 
+class CustomOpenAIClientConfig(OpenAIClientConfig):
+    chat_model: Literal[
+        "gpt-4-0125-preview",
+        "gpt-4-1106-preview",
+        "gpt-4-0613",
+        "gpt-4-32k-0613",
+    ] = Field(default=...)
+
+
+class CustomAzureOpenAIClientConfig(AzureOpenAIClientConfig):
+    chat_model: str = Field(default=...)
+
+
+class CustomOpenAIBackendConfig(OpenAIBackendConfig):
+    client_config: Union[
+        CustomOpenAIClientConfig,
+        CustomAzureOpenAIClientConfig,
+    ] = Field(default=..., union_mode="smart")
+
+
 class OpenAIBasicExamineeConfig(AIBaseExamineeConfig):
-    ai_backend_config: OpenAIBackendConfig = Field(default=...)
-    ai_backend_obj: DynamicObject = Field(
-        default=DynamicObject(obj="OpenAIBackend", module="leaf_playground.ai_backend.openai"),
-        exclude=True
-    )
+    ai_backend_config: CustomOpenAIBackendConfig = Field(default=...)
+    ai_backend_cls: Type[OpenAIBackend] = Field(default=OpenAIBackend)
 
 
 class OpenAIBasicExaminee(AIBaseExaminee, cls_description="Examinee agent using OpenAI API to answer questions"):
@@ -35,9 +51,7 @@ class OpenAIBasicExaminee(AIBaseExaminee, cls_description="Examinee agent using 
         super().__init__(config=config)
 
     async def answer_question(self, question: ExaminerQuestion, examiner: Profile) -> ExamineeAnswer:
-        client: AsyncOpenAI = self.backend.async_client
-        model = self.config.ai_backend_config.model
-        is_chat_model = model in CHAT_MODELS
+        model = self.config.ai_backend_config.chat_model
 
         system_msg = (
             f"You are a meticulous scholar who, when faced with users' questions, not only accurately answers their "
@@ -50,67 +64,41 @@ class OpenAIBasicExaminee(AIBaseExaminee, cls_description="Examinee agent using 
         )
         examiner_msg = question.content.text
 
-        if is_chat_model:
-            resp_format = {"type": "json_object"} if model.find("gpt-4") >= 0 else {"type": "text"}
-            try:
-                resp = await client.chat.completions.create(
-                    messages=[
-                        ChatCompletionSystemMessageParam(role="system", content=system_msg),
-                        ChatCompletionUserMessageParam(role="user", content=examiner_msg),
-                    ],
-                    model=model,
-                    response_format=resp_format,
-                    max_tokens=2048
-                )
-            except Exception as e:
-                print(e)
-                resp = None
-
-            try:
-                obj = json.loads(resp.choices[0].message.content) if resp else {}
-            except Exception as e:
-                print(f'Response Not JSON: {e}')
-                obj = {
-                    "answer": resp.choices[0].message.content,
-                    "contexts": ['nothing found']  # default for ragas data type validation
-                }
-
-            contexts = obj['contexts'] if resp else ['nothing found']
-            answer = obj['answer'] if resp else ""
-
-            json_data = {"answer": answer, "contexts": contexts}
-
-            return ExamineeAnswer(
-                question_id=question.question_id,
-                content=Json(data=json_data, display_text=answer),
-                sender=self.profile,
-                receivers=[examiner]
+        resp_format = {"type": "json_object"} if model.find("gpt-4") >= 0 else {"type": "text"}
+        try:
+            resp = await self.backend.async_client.chat.completions.create(
+                messages=[
+                    ChatCompletionSystemMessageParam(role="system", content=system_msg),
+                    ChatCompletionUserMessageParam(role="user", content=examiner_msg),
+                ],
+                model=model,
+                response_format=resp_format,
+                max_tokens=2048
             )
-        else:
-            prompt = (
-                f"{system_msg}\n\n"
-                f"{examiner.name}({examiner.role.name}): {examiner_msg}\n"
-                f"{self.name}({self.role_name}): the answer is "
-            )
-            contexts = ['nothing found']
-            answer = ""
+        except Exception as e:
+            print(e)
+            resp = None
 
-            try:
-                resp = await client.completions.create(
-                    prompt=prompt,
-                    model=model,
-                    max_tokens=2048
-                )
-                answer = resp.choices[0].text if resp else ""
+        try:
+            obj = json.loads(resp.choices[0].message.content) if resp else {}
+        except Exception as e:
+            print(f'Response Not JSON: {e}')
+            obj = {
+                "answer": resp.choices[0].message.content,
+                "contexts": ['nothing found']  # default for ragas data type validation
+            }
 
-            except Exception as e:
-                pass
-            return ExamineeAnswer(
-                question_id=question.question_id,
-                content=Json(data={"answer": answer, "contexts": contexts}, display_text=answer),
-                sender=self.profile,
-                receivers=[examiner]
-            )
+        contexts = obj['contexts'] if resp else ['nothing found']
+        answer = obj['answer'] if resp else ""
+
+        json_data = {"answer": answer, "contexts": contexts}
+
+        return ExamineeAnswer(
+            question_id=question.question_id,
+            content=Json(data=json_data, display_text=answer),
+            sender=self.profile,
+            receivers=[examiner]
+        )
 
 
 __all__ = [
